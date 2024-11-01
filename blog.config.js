e64311ec7fc220c897077f54608e06fef686d338
LisnX <53891402+LisnXy@users.noreply.github.com>
2024-11-01 12:50:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -89,8 +89,7 @@ const BLOG = {
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    // '"LXGW WenKai"',
-    'Bitter',
+    '"Noto Serif"'
     '"Noto Serif SC"',
     'SimSun',
     '"Times New Roman"',
