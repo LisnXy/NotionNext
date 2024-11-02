4ce84704071a16cc7916556f08b67ce8c660d988
LisnX <53891402+LisnXy@users.noreply.github.com>
2024-11-02 22:06:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -68,37 +68,40 @@ const BLOG = {
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
-  FONT_SANS: [
-    '"Noto Sans"',
-    '"Noto Sans SC"',
-    '"PingFang SC"',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    '"Hiragino Sans GB"',
-    '"Microsoft YaHei"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-    '"Segoe UI"',
-    'HarmonyOS_Regular',
-    '"Helvetica Neue"',
-    'Helvetica',
-    '"Source Han Sans SC"',
-    'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"'
-  ],
-  // 衬线字体 例如'"LXGW WenKai"'
-  FONT_SERIF: [
-    '"Noto Serif"',
-    '"Noto Serif SC"',
-    'SimSun',
-    '"Times New Roman"',
-    'Times',
-    'serif',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-    '"Apple Color Emoji"'
-  ],
+FONT_SANS: [
+  // '"LXGW WenKai"',
+  '"PingFang SC"',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Hiragino Sans GB"',
+  '"Microsoft YaHei"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Segoe UI"',
+  '"Noto Sans SC"',
+  'HarmonyOS_Regular',
+  '"Helvetica Neue"',
+  'Helvetica',
+  '"Source Han Sans SC"',
+  'Arial',
+  'sans-serif',
+  '"Apple Color Emoji"'
+],
+
+
+// 衬线字体 例如'"LXGW WenKai"'
+FONT_SERIF: [
+  // '"LXGW WenKai"',
+  'Bitter',
+  '"Noto Serif SC"',
+  'SimSun',
+  '"Times New Roman"',
+  'Times',
+  'serif',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Apple Color Emoji"'
+],
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
